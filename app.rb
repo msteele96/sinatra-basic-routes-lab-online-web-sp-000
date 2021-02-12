5e00e00911fc5754f88eb2d82d992d20cf7e0589
msteele96 <marcsteele2014@gmail.com>
2021-02-12 12:12:01 -0500

    commit tree 8f125d9b1e6c23ed347d39bd1891396d80089268

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/hometown' do
+        "My hometown is Danville, NH."
+    end
+    
+    get '/name' do
+        "My name is Marc Steele."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Sand in my Boots"
+    end
+
 end
